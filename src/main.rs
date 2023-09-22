f9287759c6f9c58aec2912d3bd5daa79790cbf7b
abkux <kumarjhaabhishek2002@gmail.com>
2023-09-22 10:53:17 +0530

    rust commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut number = 5;
+    println!("Number is : { }", number);
+    number = 7;
+    println!("Number is : { }", number);
+
+    const SECONDS: u32 = 60;
+    println!("Seconds : { }", SECONDS);
+
+    let spaces = "    ";
+    println!("{ }", spaces.len());
+}
